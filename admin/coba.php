3d56a825fca77baea73d40b3735c24cbfe204224
P3D0 <ryanarya1238@gmail.com>
2022-07-02 13:20:25 +0700

    bingbang

---
+++ admin/coba.php
@@ -0,0 +1,257 @@
+<?php
+session_start();
+include '../dbconnect.php';
+
+$idproduk = $_GET['idproduk'];
+$query = UPDATE MyGuests SET lastname='Doe' WHERE id=2
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+	<link rel="icon" 
+      type="image/png" 
+      href="../favicon.png">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Admin Panel - Tokopekita</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/png" href="assets/images/icon/favicon.ico">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/metisMenu.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/slicknav.min.css">
+	
+    <!-- amchart css -->
+    <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
+    <!-- others css -->
+    <link rel="stylesheet" href="assets/css/typography.css">
+    <link rel="stylesheet" href="assets/css/default-css.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <!-- modernizr css -->
+    <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <!-- preloader area start -->
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+    <!-- preloader area end -->
+    <!-- page container area start -->
+    <div class="page-container">
+        <!-- sidebar menu area start -->
+        <div class="sidebar-menu">
+            <div class="main-menu">
+                <div class="menu-inner">
+                    <nav>
+                        <ul class="metismenu" id="menu">
+							<li class="active"><a href="index.php"><span>Home</span></a></li>
+							<li><a href="../"><span>Kembali ke Toko</span></a></li>
+							<li>
+                                <a href="manageorder.php"><i class="ti-dashboard"></i><span>Kelola Pesanan</span></a>
+                            </li>
+							<li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layout"></i><span>Kelola Toko
+                                    </span></a>
+                                <ul class="collapse">
+                                    <li><a href="kategori.php">Kategori</a></li>
+                                    <li><a href="produk.php">Produk</a></li>
+									<li><a href="pembayaran.php">Metode Pembayaran</a></li>
+                                </ul>
+                            </li>
+							<li><a href="customer.php"><span>Kelola Pelanggan</span></a></li>
+							<li><a href="user.php"><span>Kelola Staff</span></a></li>
+                            <li>
+                                <a href="../logout.php"><span>Logout</span></a>
+                                
+                            </li>
+                            
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <!-- sidebar menu area end -->
+        <!-- main content area start -->
+        <div class="main-content">
+            <!-- header area start -->
+            <div class="header-area">
+                <div class="row align-items-center">
+                    <!-- nav and search button -->
+                    <div class="col-md-6 col-sm-8 clearfix">
+                        <div class="nav-btn pull-left">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                    <!-- profile info & task notification -->
+                    <div class="col-md-6 col-sm-4 clearfix">
+                        <ul class="notification-area pull-right">
+                            <li><h3><div class="date">
+								<script type='text/javascript'>
+						var months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+						var myDays = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
+						var date = new Date();
+						var day = date.getDate();
+						var month = date.getMonth();
+						var thisDay = date.getDay(),
+							thisDay = myDays[thisDay];
+						var yy = date.getYear();
+						var year = (yy < 1000) ? yy + 1900 : yy;
+						document.write(thisDay + ', ' + day + ' ' + months[month] + ' ' + year);		
+						//-->
+						</script></b></div></h3>
+
+						</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+			
+			
+			<!-- header area end -->
+			<?php 
+			/*
+				$periksa_bahan=mysqli_query($conn,"select * from stock_brg where stock <10");
+				while($p=mysqli_fetch_array($periksa_bahan)){	
+					if($p['stock']>=1){	
+						?>	
+						<script>
+							$(document).ready(function(){
+								$('#pesan_sedia').css("color","white");
+								$('#pesan_sedia').append("<i class='ti-flag'></i>");
+							});
+						</script>
+						<?php
+						echo "<div class='alert alert-danger alert-dismissible fade show'><button type='button' class='close' data-dismiss='alert'>&times;</button>Stok  <strong><u>".$p['nama']. "</u> <u>".($p['jenis'])."</u></strong> yang tersisa kurang dari 10</div>";		
+					}
+				}
+				
+				*/
+				?>
+			
+            
+            <!-- page title area end -->
+            <div class="main-content-inner">
+			
+                
+                
+                
+                
+                <!-- overview area end -->
+                <!-- market value area start -->
+                <div class="row mt-5 mb-5">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                            <?php 
+				$p = mysqli_fetch_array(mysqli_query($conn,"Select * from produk where idproduk='$idproduk'"));
+				echo $p['namaproduk'];
+				?>
+                                    <form action="produk.php" method="post" enctype="multipart/form-data" >
+                                        <div class="form-group">
+                                            <label>Nama Produk</label>
+                                            <input name="namaproduk" type="text" class="form-control" required autofocus value="<?php echo $p['namaproduk'] ?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Nama Kategori</label>
+                                            <select name="idkategori" class="form-control">
+                                            <option selected>Pilih Kategori</option>
+                                            <?php
+                                            $det=mysqli_query($conn,"select * from kategori order by namakategori ASC")or die(mysqli_error());
+                                            while($d=mysqli_fetch_array($det)){
+                                            ?>
+                                                <option value="<?php echo $d['idkategori'] ?>"><?php echo $d['namakategori'] ?></option>
+                                                <?php
+                                        }
+                                        ?>		
+                                            </select>
+                                            
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Deskripsi</label>
+                                            <input name="deskripsi" type="text" class="form-control" required value="<?php echo $p['deskripsi'] ?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Rating (1-5)</label>
+                                            <input name="rate" type="number" class="form-control"  min="1" max="5" required value="<?php echo $p['rate'] ?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Harga Sebelum Diskon</label>
+                                            <input name="hargabefore" type="number" class="form-control" value="<?php echo $p['hargabefore'] ?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Harga Setelah Diskon</label>
+                                            <input name="hargaafter" type="number" class="form-control" value="<?php echo $p['hargaafter'] ?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Gambar</label>
+                                            <input name="uploadgambar" type="file" class="form-control" value="<?php echo $p['gambar'] ?>">
+                                        </div>
+
+                                    </div>
+                                    <div class="modal-footer">
+                                        <input name="editproduk" type="submit" class="btn btn-primary" value="Edit Produk">
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              
+                
+                <!-- row area start-->
+            </div>
+        </div>
+        <!-- main content area end -->
+		
+		
+		
+        <!-- footer area start-->
+        <footer>
+            <div class="footer-area">
+                <p>© 2022 PetsQuShop's</p>
+            </div>
+        </footer>
+        <!-- footer area end-->
+    </div>
+    <!-- page container area end -->
+
+    <!-- jquery latest version -->
+    <script src="assets/js/vendor/jquery-2.2.4.min.js"></script>
+    <!-- bootstrap 4 js -->
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/metisMenu.min.js"></script>
+    <script src="assets/js/jquery.slimscroll.min.js"></script>
+    <script src="assets/js/jquery.slicknav.min.js"></script>
+
+    <!-- start chart js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
+    <!-- start highcharts js -->
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <!-- start zingchart js -->
+    <script src="https://cdn.zingchart.com/zingchart.min.js"></script>
+    <script>
+    zingchart.MODULESDIR = "https://cdn.zingchart.com/modules/";
+    ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "ee6b7db5b51705a13dc2339db3edaf6d"];
+    </script>
+    <!-- all line chart activation -->
+    <script src="assets/js/line-chart.js"></script>
+    <!-- all pie chart -->
+    <script src="assets/js/pie-chart.js"></script>
+    <!-- others plugins -->
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/scripts.js"></script>
+</body>
+
+</html>
